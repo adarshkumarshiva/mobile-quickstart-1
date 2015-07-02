ce44a1f5ad9e1b493630d3d37299119779de01e8
Adarsh Kumar Shiva <adarshkumarshiva@gmail.com>
2015-07-02 11:21:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -30,7 +30,7 @@ def token():
   # This allows incoming connections to client (if specified)
   client = request.values.get('client')
   if client != None:
-    capability.allow_client_incoming("cupola")
+    capability.allow_client_incoming("cupolaConference")
 
   # This returns a token to use with Twilio based on the account and capabilities defined above
   return capability.generate()
